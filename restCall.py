1d2d599b331de1d0233ef1ecf42ea371055c7c10
Raj <rajsagar.rawool@mavs.uta.edu>
2018-02-22 22:50:15 -0600

    Update restCall.py

--- restCall.py
+++ restCall.py
@@ -7,8 +7,7 @@ import urllib2
 i_ticker = "C_USA_CHN"
 i_field = "EXPORT"
 i_source = "USDA"
-i_key = "7878787"
-
+i_key = "ENTER_KEY"
 
 link = 'http://localhost:5000/data?ticker=' + i_ticker
 link = link + '&field=' + i_field + '&source=' + i_source
